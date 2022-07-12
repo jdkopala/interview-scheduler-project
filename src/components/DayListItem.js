b4404b8e8004c8e00d3cd9470215740ac781509e
Jesse Kopala <jdkopala@gmail.com>
2022-07-12 10:18:48 -0600

    Create DayList and stories for testing. Refactor DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
+  let name = props.name;
+  let spots = props.spots;
+  let setDay = props.setDay;
+  let listItemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': spots === 0
+  });
+  
   return(
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => setDay(name)} className={listItemClass}>
+      <h2 className="text--regular">{name}</h2>
+      {spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {spots === 1 && <h3 className="text--light">{spots} spot remaining</h3>}
+      {spots >= 1 && <h3 className="text--light">{spots} spots remaining</h3>}
     </li>
   );
 };
