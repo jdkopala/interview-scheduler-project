2c18a97986629965746f87f4ee34e8d10c4bc29b
Jesse Kopala <jdkopala@gmail.com>
2022-07-13 16:56:30 -0600

    Implement schedule rendering on whole application with hard coded data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import "./styles.scss"
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+
 
 const Appointment = (props) => {
 
   return(
     <article className='appointment'>
       <Header time={props.time} />
-      {props.interview ? <Show /> : <Empty />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
     </article>
   );
 };
