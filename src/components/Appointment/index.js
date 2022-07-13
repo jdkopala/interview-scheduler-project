8f5ee0b76f68cf0100812bee85a689a16922dc5d
Jesse Kopala <jdkopala@gmail.com>
2022-07-13 09:58:40 -0600

    Begin creation of Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Appointment from 'components/Appointment';
+
+const Appointment = (props) => {
+
+  return(
+    <article className='appointment'>
+
+    </article>
+  );
+};
+
+export default Appointment;
