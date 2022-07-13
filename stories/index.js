bba05fce0ba4e2284e008c90ff903b2dcb875c14
Jesse Kopala <jdkopala@gmail.com>
2022-07-13 13:44:56 -0600

    Setup Appointment/Form component and story, feature is WIP

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -188,4 +189,24 @@ storiesOf('Appointment', module)
       message="Could not delete appointment"
       onClose={action('onClose')}
       />
-    ))
+    ));
+
+storiesOf('Form', module)
+  .addParameters({
+    backgrounds: [{ name: 'white', value: '#fff', default: true}]
+  })
+  .add('Edit', () => (
+    <Form
+    student="Jesse Kopala"
+    interviewer={3}
+    interviewers={interviewers}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
+    />
+  ))
+  .add('Create', () => (
+    <Form
+    interviewers={interviewers}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
+  ))
