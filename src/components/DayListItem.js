4914760fcd495f047cfc9ba3d937a09c739dfdce
Jesse Kopala <jdkopala@gmail.com>
2022-07-19 18:27:02 -0600

    Implement mock axios for jest, and tests for Appointment component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -13,7 +13,7 @@ export default function DayListItem(props) {
   });
   
   return(
-    <li onClick={() => setDay(name)} className={listItemClass} selected={props.selected}>
+    <li onClick={() => setDay(name)} className={listItemClass} selected={props.selected} data-testid="day">
       <h2 className="text--regular">{name}</h2>
       {spots === 0 && <h3 className="text--light">no spots remaining</h3>}
       {spots === 1 && <h3 className="text--light">{spots} spot remaining</h3>}
